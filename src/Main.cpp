7312ef2839e7234778d7bb90607a09def2cec5d1
Tyler Trinh <tylervtrinh@gmail.com>
2020-08-10 22:05:12 -0700

    Include separate iterations and time for each AI

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 // Play a game of Reversi
-void startGame(vector<tuple<char, int, int, int, double>> &results, int index, int p1Type, 
+void startGame(vector<tuple<char, int, int, int, int, double, double, double>> &results, int index, int p1Type, 
     int p2Type, bool display=false) {
     srand(time(0));
 
@@ -22,6 +22,7 @@ void startGame(vector<tuple<char, int, int, int, double>> &results, int index, i
     Reversi game;
     Move move1;
     Move move2;
+    int white, black;
     vector<Move> moves1 = game.listMoves(state->getState(), 'T', 'F');
     vector<Move> moves2 = game.listMoves(state->getState(), 'F', 'T');
 
@@ -32,8 +33,7 @@ void startGame(vector<tuple<char, int, int, int, double>> &results, int index, i
     while (true) {
 
         if (moves1.size() > 0) {
-            // move1 = p1.getMove(game, state, moves1);
-            move1 = findBestMove(game, state, moves1);
+            move1 = p1.getMove(game, state, moves1);
             game.makeMove(state, move1);
         }
         state->togglePlayer();
@@ -66,22 +66,24 @@ void startGame(vector<tuple<char, int, int, int, double>> &results, int index, i
     auto end = chrono::high_resolution_clock::now();
     double totalTime = chrono::duration_cast<chrono::duration<double>>(end - start).count();
 
-    int white, black;
+    // Get the final score of the game
     tie(white, black) = game.score(state->getState());
+
+    // Return the winner, score, iterations and time for each search algorithm and the total time
     results[index] = make_tuple(game.winningPlayer(state->getState()), white, black, 
-        state->getIterations(), totalTime);
+        p1.iterations, p2.iterations, p1.searchTime, p2.searchTime, totalTime);
     delete state;
 }
 
 // Display the winner, score, number of iterations used in MCTS, length of game (s)
-void displayResults(vector<tuple<char, int, int, int, double>> results, double const time,
-    int const numGames) {
+void displayResults(vector<tuple<char, int, int, int, int, double, double, double>> results, 
+    double const time, int const numGames) {
 
     char winner;
     int wWins = 0;
     int bWins = 0;
 
-    printf("WINNER\t\tSCORE(W-B)\t\tITERATIONS\t\tTIME\n");
+    printf("WINNER\t\tSCORE(W-B)\t\tP1_ITERATIONS\t\tP2_ITERATIONS\t\tP1_TIME\t\tP2_TIME\t\tTOTAL_TIME\n");
     for (auto &res:results) {
         winner = get<0>(res);
         if (winner == 'T')
@@ -89,8 +91,8 @@ void displayResults(vector<tuple<char, int, int, int, double>> results, double c
         else if (winner == 'F')
             bWins += 1;
 
-        printf("%c\t\t\t%d-%d\t\t\t%d\t\t%f\n", winner, get<1>(res), get<2>(res), get<3>(res),
-            get<4>(res));
+        printf("%c\t\t\t%d-%d\t\t\t%d\t\t%d\t\t %f\t\t%f\t\t%f\n", winner, get<1>(res), get<2>(res),
+            get<3>(res), get<4>(res), get<5>(res), get<6>(res), get<7>(res));
     }
 
     printf("Player1Wins: %d\t\t Player2Wins: %d\n", wWins, bWins);
@@ -102,22 +104,29 @@ void displayResults(vector<tuple<char, int, int, int, double>> results, double c
 void simulateGames(int const numGames, int const p1, int const p2, bool const display) {
 
     vector<thread> threads;
-    vector<tuple<char, int, int, int, double>> results(numGames);
+    vector<tuple<char, int, int, int, int, double, double, double>> results(numGames);
 
     auto start = chrono::high_resolution_clock::now();
 
     printf("Playing %d games\n", numGames);
-    for (int i = 0; i < numGames; i++)
-        threads.push_back(thread(startGame, ref(results), i, p1, p2, display));
 
-    for (auto &t:threads) {
-        if (t.joinable())
-            t.join();
+    if (numGames == 1) {
+        startGame(results, 0, p1, p2, display);
+    }
+    else {
+        for (int i = 0; i < numGames; i++)
+            threads.push_back(thread(startGame, ref(results), i, p1, p2, display));
+
+        for (auto &t:threads) {
+            if (t.joinable())
+                t.join();
+        }
     }
     auto end = chrono::high_resolution_clock::now();
     double duration = chrono::duration_cast<chrono::duration<double>>(end - start).count();
 
     displayResults(results, duration, numGames);
+    printf("P1: %d P2: %d\n", p1, p2);
 }
 
 int main(int argc, char* argv[]) {
@@ -126,11 +135,11 @@ int main(int argc, char* argv[]) {
     cxxopts::Options options("./game", "A Reversi game using the MCTS algorithm");
     options.add_options()
         ("p,player", "Choose what algorithm to use to select a move: 0-pureMCTS, 1-improvedMCTS, "
-            "2-inputPlayer, 3-randomPlayer", cxxopts::value<int>()->default_value("3"))
-        ("o,opponent", "Choose what algorithm to use to select a move: 0-pureMCTS, 1-improvedMCTS, "
             "2-inputPlayer, 3-randomPlayer", cxxopts::value<int>()->default_value("0"))
+        ("o,opponent", "Choose what algorithm to use to select a move: 0-pureMCTS, 1-improvedMCTS, "
+            "2-inputPlayer, 3-randomPlayer", cxxopts::value<int>()->default_value("1"))
         ("n,numgames", "The number of games to be ran", cxxopts::value<int>()->
-            default_value("50"))
+            default_value("20"))
         ("h,help","Print help and exit")
         ;
     auto result = options.parse(argc, argv);
